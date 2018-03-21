ecab5cb8c0844d1e2070651fed7984a65de01a3c
pestovpvl <pestovpvl@mail.ru>
2018-03-20 23:16:08 -0400

    Made table for master

--- app.rb
+++ app.rb
@@ -3,6 +3,19 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sqlite3'
 
+def is_barber_exists? db, name
+  db.execute('select * from Barbers where name=?', [name]).length > 0
+end
+
+
+def seed_db db, barbers
+    barbers.each do |barber|
+      if !is_barber_exists? db, barber
+        db.execute 'insert into Barbers (name) values (?)', [barber]
+      end
+    end
+end
+
 def get_db
   return SQLite3::Database.new 'barbershop.db'
   db.results_as_hash = true
@@ -22,6 +35,16 @@ db.execute 'CREATE TABLE IF NOT EXISTS
    "barber" TEXT,
    "color" TEXT
  )'
+
+
+ db.execute 'CREATE TABLE IF NOT EXISTS
+              "Barbers"
+   (
+     "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+     "name" TEXT
+ )'
+
+seed_db db, ['Jessie Pikman', 'Walter White', 'Gus Fring', 'Mike']
 end
 
 helpers do
@@ -126,4 +149,13 @@ end
 
 get '/contact' do
   "Hello World"
+end
+
+get '/showusers' do
+  db = get_db
+
+  @results = db.execute 'select * from Users order by id desc'
+
+
+  erb :showusers
 end
